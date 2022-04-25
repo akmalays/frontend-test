3bd2f5a8237cc012ad00918bffec31879cbcc76b
akmalays <akmalays@gmail.com>
2022-04-25 23:48:21 +0700

    fixing some detail

--- src/components/sidenav.jsx
+++ src/components/sidenav.jsx
@@ -7,6 +7,8 @@ import TabPane from "@material-tailwind/react/TabPane";
 import { GoGlobe } from "react-icons/go";
 import { GiLion } from "react-icons/gi";
 import { FaPhotoVideo } from "react-icons/fa";
+import { BsChatLeftTextFill } from "react-icons/bs";
+import { MdNotificationImportant } from "react-icons/md";
 
 function Sidenav() {
   const [openTab, setOpenTab] = useState(1);
@@ -19,7 +21,7 @@ function Sidenav() {
             className="max-w-[200px]  h-screen rounded-none bg-sky-300"
             color="blueGray"
           >
-            <div className="grid h-[400px]">
+            <div className="grid h-[400px] ">
               <TabItem
                 onClick={(e) => {
                   e.preventDefault();
@@ -30,8 +32,8 @@ function Sidenav() {
                 href="tabItem"
                 className="flex-col"
               >
-                <GoGlobe />
-                Browse
+                <GoGlobe size={40} />
+                <p className="text-xs"> Browse</p>
               </TabItem>
               <TabItem
                 onClick={(e) => {
@@ -43,8 +45,8 @@ function Sidenav() {
                 href="tabItem"
                 className="flex-col"
               >
-                <GiLion />
-                Suggest Attraction
+                <GiLion size={40} />
+                <p className="text-xs"> Suggest Attraction</p>
               </TabItem>
               <TabItem
                 onClick={(e) => {
@@ -56,8 +58,8 @@ function Sidenav() {
                 href="tabItem"
                 className="flex-col"
               >
-                <FaPhotoVideo />
-                Videos
+                <FaPhotoVideo size={40} />
+                <p className="text-xs">Videos</p>
               </TabItem>
               <TabItem
                 onClick={(e) => {
@@ -69,7 +71,8 @@ function Sidenav() {
                 href="tabItem"
                 className="flex-col"
               >
-                Blog
+                <BsChatLeftTextFill size={40} />
+                <p className="text-xs">Blog</p>
               </TabItem>
               <TabItem
                 onClick={(e) => {
@@ -81,22 +84,22 @@ function Sidenav() {
                 href="tabItem"
                 className="flex-col"
               >
-                About
+                <MdNotificationImportant size={40} />
+                <p className="text-xs"> About</p>
               </TabItem>
             </div>
           </TabList>
 
           <TabContent className="bg-gray-800">
             <TabPane active={openTab === 1 ? true : false}>
-              <p>
-                I think that’s a responsibility that I have, to push
-                possibilities, to show people, this is the level that things
-                could be at. So when you get something that has the name Kanye
-                West on it, it’s supposed to be pushing the furthest
-                possibilities. I will be the leader of a company that ends up
-                being worth billions of dollars, because I got the answers. I
-                understand culture. I am the nucleus.
-              </p>
+              <div className="space-y-5">
+                <p className="font-bold">filter by favourite</p>
+                <p className="font-bold">Merlion</p>
+                <p className="font-bold">Marina Baysands</p>
+                <p className="font-bold">Garden by the bay</p>
+                <p className="font-bold">China Town</p>
+                <p className="font-bold">clarke quay</p>
+              </div>
             </TabPane>
             <TabPane active={openTab === 2 ? true : false}>
               <p>
